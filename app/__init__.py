5c5f0611930573f61c2e11474e2489612569b170
gleuber lucio <gleuberlucio@gmail.com>
2025-05-31 18:03:54 -0300

    variavel app como global

--- app/__init__.py
+++ app/__init__.py
@@ -3,9 +3,9 @@ from flask_sqlalchemy import SQLAlchemy
 from config.config import Config
 
 db = SQLAlchemy()
+app = Flask(__name__)
 
 def init_app():
-    app = Flask(__name__)
     app.config.from_object(Config)
     
     db.init_app(app)
